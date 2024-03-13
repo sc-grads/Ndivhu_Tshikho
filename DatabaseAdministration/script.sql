a4dcfc38e8882cedcfb84613a62e86b4a970b6a4
ndivhudza1 <160632015+ndivhudza1@users.noreply.github.com>
2024-03-14 01:48:00 +0200

    Update script.sql

--- DatabaseAdministration/script.sql
+++ DatabaseAdministration/script.sql
@@ -1,7 +1,10 @@
--- Create the database
-CREATE DATABASE MyDatabase;
-
--- Use the created database
+-- Create MyDatabase if it doesn't exist
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'MyDatabase')
+BEGIN
+    CREATE DATABASE MyDatabase;
+END
+GO
+-- Your existing SQL commands here
 USE MyDatabase;
 
 -- Create the Person table
