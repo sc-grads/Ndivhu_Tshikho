ddab61e26bb7f46821713ebafcf4b6e88991c54a
ndivhudza1 <160632015+ndivhudza1@users.noreply.github.com>
2024-03-14 23:48:28 +0200

    Update script.sql

--- DatabaseAdministration/SQLStatement/script.sql
+++ DatabaseAdministration/SQLStatement/script.sql
@@ -7,24 +7,29 @@ GO
 
 USE MyDatabase;
 
--- Create a stored procedure to insert data into the Person table
-CREATE PROCEDURE InsertPerson
-    @FirstName NVARCHAR(50),
-    @LastName NVARCHAR(50)
-AS
-BEGIN
-    INSERT INTO Person (FirstName, LastName) VALUES (@FirstName, @LastName);
-END
-GO
-
 -- Create the Person table
 CREATE TABLE Person (
         FirstName NVARCHAR(50),
         LastName NVARCHAR(50)
     );
 
+-- Create a stored procedure to insert data into the Person table
+IF NOT EXISTS (SELECT * FROM sys.procedures WHERE name = 'InsertPerson')
+BEGIN
+    EXEC('
+    CREATE PROCEDURE InsertPerson
+        @FirstName NVARCHAR(50),
+        @LastName NVARCHAR(50)
+    AS
+    BEGIN
+        INSERT INTO Person (FirstName, LastName) VALUES (@FirstName, @LastName);
+    END
+    ');
+END
+GO
+
 -- Insert sample data into the Person table
-INSERT INTO Person(FirstName, LastName) VALUES('John', 'Doe'),('Jane', 'Smith'),('Alice', 'Johnson');
+-- INSERT INTO Person(FirstName, LastName) VALUES('John', 'Doe'),('Jane', 'Smith'),('Alice', 'Johnson');
 
 -- Use the stored procedure to insert more data
 EXEC InsertPerson 'Michael', 'Jordan';
