9f7ef727687d7035cfdd41d1d82d1b9211d68526
ndivhudza1 <ndivhudzannyi.tshikhovhokhovho@sambeconsulting.com>
2024-08-28 11:16:30 +0200

    "Angular frontend for appointment"

---
+++ Development/Ecommerce/Frontend/my-angular-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
