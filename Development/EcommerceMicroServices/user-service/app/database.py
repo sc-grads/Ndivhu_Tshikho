b3b1b0417c85ac2d87665b3297ef70986122ecc9
ndivhudza1 <ndivhudzannyi.tshikhovhokhovho@sambeconsulting.com>
2024-10-16 12:56:57 +0200

    Shared database using sqlite

--- Development/EcommerceMicroServices/user-service/app/database.py
+++ Development/EcommerceMicroServices/user-service/app/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./user.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////data/ecommerce.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
